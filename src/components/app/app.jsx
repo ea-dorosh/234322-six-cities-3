68817d12067680f56ac08d554cd5dcd2091c25c3
Egor <dorosh.ea@yandex.ru>
2020-02-08 22:36:13 +0800

    start first task

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,10 +4,16 @@ import Main from "../main/main.jsx";
 
 const App = (props) => {
   // eslint-disable-next-line react/prop-types
-  const {advertsCount} = props;
+  const advertsCount = props.advertsCount;
+  // eslint-disable-next-line react/prop-types
+  const placesName = props.placesNames;
+  console.log(placesName)
 
   return (
-    <Main advertsCount = {advertsCount}/>
+    <Main
+      advertsCount = {advertsCount}
+      placeName = {placesName}
+    />
   );
 };
 
