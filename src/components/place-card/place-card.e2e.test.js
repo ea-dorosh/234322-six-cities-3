dec63984c869b4e91d1dde46f6117d0bc6a57d6a
Egor <dorosh.ea@yandex.ru>
2020-02-09 18:49:14 +0800

    complete module2-task2

---
+++ src/components/place-card/place-card.e2e.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Enzyme, {shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import PlaceCard from "./place-card.jsx";
+
+Enzyme.configure({
+  adapter: new Adapter(),
+});
+
+it(`Should Place Name Header be pressed`, () => {
+  const onPlaceNameHeaderClick = jest.fn();
+
+  const placeCard = shallow(
+      <PlaceCard
+        placesNames={`some string`}
+        onPlaceNameHeaderClick={onPlaceNameHeaderClick}
+      />
+  );
+
+  const placeNameHeaderButton = placeCard.find(`h2.place-card__name`);
+
+  placeNameHeaderButton.props().onClick();
+
+  expect(onPlaceNameHeaderClick.mock.calls.length).toBe(1);
+});
